0fb3545c36978742a6df376789f91d27a25b72df
Lukas <lukas.kornmayer@web.de>
2026-02-13 11:27:24 +0100

    complete collision detection

--- courses/coding-in-C/Lab_5/lab5.c
+++ courses/coding-in-C/Lab_5/lab5.c
@@ -2,35 +2,50 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
-    int field[] = {0,1,0,1,0,1,0,0,0,0};
-    int size = sizeof(field) / sizeof(*field);
+void particle_simulation(int *pField, int *pTemp, int size) {
 
-    int temp[size];
-    int *pField = field;
-    int *pTemp  = temp;
+    for (int i = 0; i < size; i++) {
+        pTemp[i] = 0;
+    }
+
+    for (int i = 0; i < size; i++) {
+        if (pField[i] == 1) {
+            if (rand() % 2 == 0) {
+                if (i > 0) {
+                    pTemp[i - 1] ^= 1;
+                } else {
+                    pTemp[i] = 1;
+                }
+            } else {
+                if (i < size - 1) {
+                    pTemp[i + 1] ^= 1;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < size; i++) {
+        pField[i] = pTemp[i];
+        printf("%d ", pField[i]);
+    }
+
+    printf("\n");
+}
+
+int main() {
+
+    int field[] = {0,1,0,1,0,1,0,0,0,0};
+    int size = sizeof(field) / sizeof(field[0]);
+
+    int *pTemp = calloc(size, sizeof(field[0]));
+    if (!pTemp) return 1;
 
     srand(time(NULL));
 
-    for (int i = 0; i < size; i++)
-        *(pTemp + i) = 0;
+    particle_simulation(field, pTemp, size);
+    particle_simulation(field, pTemp, size);
+    particle_simulation(field, pTemp, size);
 
-    for (int i = 0; i < size; i++) {
-        if (*(pField + i) == 1) {
-
-            if (rand() % 2 == 0) {
-                if (i > 0)
-                    *(pTemp + i - 1) = 1;
-            } else {
-                if (i < size - 1)
-                    *(pTemp + i + 1) = 1;
-            }
-        } 
-    }
-
-    for (int i = 0; i < size; i++)
-        *(pField + i) = *(pTemp + i);
-
-    for (int i = 0; i < size; i++)
-        printf("%d", *(pField + i));
+    free(pTemp);
+    return 0;
 }
